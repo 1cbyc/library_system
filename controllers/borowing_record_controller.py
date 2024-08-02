2af87d9049ba98f8d208c2936b69224c85ca1fbf
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:21:29 +0100

    structured the record controller logic

---
+++ controllers/borowing_record_controller.py
@@ -0,0 +1,14 @@
+from models.borrowing_record import BorrowingRecord
+from utils.database import Database
+
+
+class BorrowingRecordController:
+    def __init__(self):
+        self.db = Database()
+
+    def add_borrowing_record(self, book_id, member_id, borrow_date, due_date, return_date=None):
+        new_record = BorrowingRecord(book_id, member_id, borrow_date, due_date, return_date)
+        self.db.add_borrowing_record(new_record)
+
+    def get_all_borrowing_records(self):
+        return self.db.get_all_borrowing_records()
