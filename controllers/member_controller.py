03a14ef706b0878c57c2e828c734c00fc14f6f7b
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:21:08 +0100

    added my member controlelr logic

---
+++ controllers/member_controller.py
@@ -0,0 +1,14 @@
+from models.member import Member
+from utils.database import Database
+
+
+class MemberController:
+    def __init__(self):
+        self.db = Database()
+
+    def add_member(self, name, email, phone):
+        new_member = Member(name, email, phone)
+        self.db.add_member(new_member)
+
+    def get_all_members(self):
+        return self.db.get_all_members()
