c627062747422e80486fcd5b05ff59a05dffb428
1cbyc <isaacnsisong@gmail.com>
2024-08-02 03:12:30 +0100

    updating the book controller

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -1,24 +1,38 @@
-# from models.book import Book
-from utils.database import Database
-from library_system.models.book import Book
+# # from models.book import Book
+# from utils.database import Database
+# from library_system.models.book import Book
+#
+#
+# class BookController:
+#     def __init__(self):
+#         self.db = Database()
+#
+#     def add_book(self, title, author, genre, isbn, copies):
+#         new_book = Book(title, author, genre, isbn, copies)
+#         self.db.add_book(new_book)
+#
+#     def get_all_books(self):
+#         return self.db.get_all_books()
+#
+#     def search_books(self, query):
+#         return self.db.search_books(query)
+#
+#     def update_book(self, book_id, **kwargs):
+#         self.db.update_book(book_id, **kwargs)
+#
+#     def delete_book(self, book_id):
+#         self.db.delete_book(book_id)
+#
 
+from library_system.models.book import Book
 
 class BookController:
     def __init__(self):
-        self.db = Database()
+        self.books = []
 
-    def add_book(self, title, author, genre, isbn, copies):
-        new_book = Book(title, author, genre, isbn, copies)
-        self.db.add_book(new_book)
+    def add_book(self, title, author, year):
+        book = Book(title, author, year)
+        self.books.append(book)
 
-    def get_all_books(self):
-        return self.db.get_all_books()
-
-    def search_books(self, query):
-        return self.db.search_books(query)
-
-    def update_book(self, book_id, **kwargs):
-        self.db.update_book(book_id, **kwargs)
-
-    def delete_book(self, book_id):
-        self.db.delete_book(book_id)
+    def display_books(self):
+        return self.books
