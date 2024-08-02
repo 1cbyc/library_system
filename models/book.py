928f54f7c3500826edac5db2c8e0956ddce051d7
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:10:27 +0100

    added logic for book model

---
+++ models/book.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, title, author, genre, isbn, copies):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.isbn = isbn
+        self.copies = copies
+
+    def __repr__(self):
+        return f"<Book(title={self.title}, author={self.author}, genre={self.genre}, isbn={self.isbn}, copies={self.copies})>"
