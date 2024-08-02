402890518cd50708d548441b38273ee05d961e07
1cbyc <isaacnsisong@gmail.com>
2024-08-02 03:12:48 +0100

    updating the member controler

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,15 +1,28 @@
-# from models.member import Member
-from utils.database import Database
-from library_system.models.member import Member
+# # from models.member import Member
+# from utils.database import Database
+# from library_system.models.member import Member
+#
+#
+# class MemberController:
+#     def __init__(self):
+#         self.db = Database()
+#
+#     def add_member(self, name, email, phone):
+#         new_member = Member(name, email, phone)
+#         self.db.add_member(new_member)
+#
+#     def get_all_members(self):
+#         return self.db.get_all_members()
 
+from library_system.models.member import Member
 
 class MemberController:
     def __init__(self):
-        self.db = Database()
+        self.members = []
 
-    def add_member(self, name, email, phone):
-        new_member = Member(name, email, phone)
-        self.db.add_member(new_member)
+    def add_member(self, name, email):
+        member = Member(name, email)
+        self.members.append(member)
 
-    def get_all_members(self):
-        return self.db.get_all_members()
+    def display_members(self):
+        return self.members
