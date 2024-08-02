ef2503c391562dce318a23480c1e924f9be8f478
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:11:50 +0100

    i got confused, so i added logic for the book view before i forgot

---
+++ views/book_view.py
@@ -0,0 +1,18 @@
+from controllers.book_controller import BookController
+
+class BookView:
+    def __init__(self):
+        self.controller = BookController()
+
+    def display_books(self):
+        books = self.controller.get_all_books()
+        for book in books:
+            print(book)
+
+    def add_book(self):
+        title = input("Enter book title: ")
+        author = input("Enter book author: ")
+        genre = input("Enter book genre: ")
+        isbn = input("Enter book ISBN: ")
+        copies = int(input("Enter number of copies: "))
+        self.controller.add_book(title, author, genre, isbn, copies)
