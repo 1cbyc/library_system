cb9aa8f240fdf44414ffcf2c724094cbae2e0d92
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:11:03 +0100

    added logic for records

---
+++ models/borrowing_record.py
@@ -0,0 +1,10 @@
+class BorrowingRecord:
+    def __init__(self, book_id, member_id, borrow_date, due_date, return_date=None):
+        self.book_id = book_id
+        self.member_id = member_id
+        self.borrow_date = borrow_date
+        self.due_date = due_date
+        self.return_date = return_date
+
+    def __repr__(self):
+        return f"<BorrowingRecord(book_id={self.book_id}, member_id={self.member_id}, borrow_date={self.borrow_date}, due_date={self.due_date}, return_date={self.return_date})>"
