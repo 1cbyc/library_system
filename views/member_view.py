ad2ec22654aff63b4e36b1081107635d5479fa10
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:21:54 +0100

    wrote member view logic

---
+++ views/member_view.py
@@ -0,0 +1,16 @@
+from controllers.member_controller import MemberController
+
+class MemberView:
+    def __init__(self):
+        self.controller = MemberController()
+
+    def display_members(self):
+        members = self.controller.get_all_members()
+        for member in members:
+            print(member)
+
+    def add_member(self):
+        name = input("Enter member name: ")
+        email = input("Enter member email: ")
+        phone = input("Enter member phone: ")
+        self.controller.add_member(name, email, phone)
