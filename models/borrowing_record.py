aca62c4bc1343fea7a034ee31f4c61cfe6a307e3
1cbyc <isaacnsisong@gmail.com>
2024-08-02 03:11:22 +0100

    done with the models - i hope it works

--- models/borrowing_record.py
+++ models/borrowing_record.py
@@ -1,10 +1,16 @@
-class BorrowingRecord:
-    def __init__(self, book_id, member_id, borrow_date, due_date, return_date=None):
-        self.book_id = book_id
-        self.member_id = member_id
-        self.borrow_date = borrow_date
-        self.due_date = due_date
-        self.return_date = return_date
+# class BorrowingRecord:
+#     def __init__(self, book_id, member_id, borrow_date, due_date, return_date=None):
+#         self.book_id = book_id
+#         self.member_id = member_id
+#         self.borrow_date = borrow_date
+#         self.due_date = due_date
+#         self.return_date = return_date
+#
+#     def __repr__(self):
+#         return f"<BorrowingRecord(book_id={self.book_id}, member_id={self.member_id}, borrow_date={self.borrow_date}, due_date={self.due_date}, return_date={self.return_date})>"
 
-    def __repr__(self):
-        return f"<BorrowingRecord(book_id={self.book_id}, member_id={self.member_id}, borrow_date={self.borrow_date}, due_date={self.due_date}, return_date={self.return_date})>"
+class BorrowingRecord:
+    def __init__(self, book_title, member_name, borrow_date):
+        self.book_title = book_title
+        self.member_name = member_name
+        self.borrow_date = borrow_date
--- models/member.py
+++ models/member.py
@@ -1,8 +1,13 @@
+# class Member:
+#     def __init__(self, name, email, phone):
+#         self.name = name
+#         self.email = email
+#         self.phone = phone
+#
+#     def __repr__(self):
+#         return f"<Member(name={self.name}, email={self.email}, phone={self.phone})>"
+
 class Member:
-    def __init__(self, name, email, phone):
+    def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.phone = phone
-
-    def __repr__(self):
-        return f"<Member(name={self.name}, email={self.email}, phone={self.phone})>"
