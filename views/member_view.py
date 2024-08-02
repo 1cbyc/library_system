5e4c82db6a59a6ddfc919f9fec0b176fa61053dd
1cbyc <isaacnsisong@gmail.com>
2024-08-02 04:09:29 +0100

    added the members view

--- views/member_view.py
+++ views/member_view.py
@@ -1,18 +1,34 @@
-# from controllers.member_controller import MemberController
-# from 1cbyc_library_system.controllers.member_controller import MemberController
+# # from controllers.member_controller import MemberController
+# # from 1cbyc_library_system.controllers.member_controller import MemberController
+# from library_system.controllers.member_controller import MemberController
+#
+# class MemberView:
+#     def __init__(self):
+#         self.controller = MemberController()
+#
+#     def display_members(self):
+#         members = self.controller.get_all_members()
+#         for member in members:
+#             print(member)
+#
+#     def add_member(self):
+#         name = input("Enter member name: ")
+#         email = input("Enter member email: ")
+#         phone = input("Enter member phone: ")
+#         self.controller.add_member(name, email, phone)
 from library_system.controllers.member_controller import MemberController
 
 class MemberView:
     def __init__(self):
         self.controller = MemberController()
 
-    def display_members(self):
-        members = self.controller.get_all_members()
-        for member in members:
-            print(member)
-
     def add_member(self):
         name = input("Enter member name: ")
         email = input("Enter member email: ")
-        phone = input("Enter member phone: ")
-        self.controller.add_member(name, email, phone)
+        self.controller.add_member(name, email)
+        print("Member added successfully.")
+
+    def display_members(self):
+        members = self.controller.display_members()
+        for member in members:
+            print(f"Name: {member.name}, Email: {member.email}")
