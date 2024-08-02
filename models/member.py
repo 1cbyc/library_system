2314f78bf96d9ed450344194b6dbcc55094294c4
1cbyc <isaacnsisong@gmail.com>
2024-08-02 02:10:42 +0100

    added logic for member model

---
+++ models/member.py
@@ -0,0 +1,8 @@
+class Member:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def __repr__(self):
+        return f"<Member(name={self.name}, email={self.email}, phone={self.phone})>"
