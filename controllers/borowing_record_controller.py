cf7c6db647d33b2e4a1bcb76ae35c862dc180564
1cbyc <isaacnsisong@gmail.com>
2024-08-02 03:13:16 +0100

    updating the record  controler

--- controllers/borowing_record_controller.py
+++ controllers/borowing_record_controller.py
@@ -1,15 +1,28 @@
-# from models.borrowing_record import BorrowingRecord
-from utils.database import Database
-from library_system.models.borrowing_record import BorrowingRecord
+# # from models.borrowing_record import BorrowingRecord
+# from utils.database import Database
+# from library_system.models.borrowing_record import BorrowingRecord
+#
+#
+# class BorrowingRecordController:
+#     def __init__(self):
+#         self.db = Database()
+#
+#     def add_borrowing_record(self, book_id, member_id, borrow_date, due_date, return_date=None):
+#         new_record = BorrowingRecord(book_id, member_id, borrow_date, due_date, return_date)
+#         self.db.add_borrowing_record(new_record)
+#
+#     def get_all_borrowing_records(self):
+#         return self.db.get_all_borrowing_records()
 
+from library_system.models.borrowing_record import BorrowingRecord
 
 class BorrowingRecordController:
     def __init__(self):
-        self.db = Database()
+        self.records = []
 
-    def add_borrowing_record(self, book_id, member_id, borrow_date, due_date, return_date=None):
-        new_record = BorrowingRecord(book_id, member_id, borrow_date, due_date, return_date)
-        self.db.add_borrowing_record(new_record)
+    def add_borrowing_record(self, book_title, member_name, borrow_date):
+        record = BorrowingRecord(book_title, member_name, borrow_date)
+        self.records.append(record)
 
-    def get_all_borrowing_records(self):
-        return self.db.get_all_borrowing_records()
+    def display_borrowing_records(self):
+        return self.records
